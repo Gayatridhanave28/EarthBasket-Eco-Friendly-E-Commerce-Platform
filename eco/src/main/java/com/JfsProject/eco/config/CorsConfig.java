7da0f689fd3e1682737b6ee1a44d3f7ce126a578
Gayatridhanave28 <gayatridhanave1@gmail.com>
2026-07-25 11:36:55 +0530

    update corsconfig file

--- eco/src/main/java/com/JfsProject/eco/config/CorsConfig.java
+++ eco/src/main/java/com/JfsProject/eco/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-				.allowedOrigins("http://localhost:5173")
+				.allowedOrigins("http://earth-basket-frontend.vercel.app")
 				.allowedMethods("*")
 				.allowCredentials(true);
 			}
